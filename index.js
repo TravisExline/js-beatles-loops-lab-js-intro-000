ce21c791e7ec65df1e8e58bfc8b9923c07d8a809
Travis Exline <t.exline22@gmail.com>
2019-04-08 17:19:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function johnLennonFacts(facts) {
   var newFacts = [];
   var i = 0;
   while (i < facts.length) {
-    newFacts[i] = facts[i] + '!!!';
+    newFacts[i] = facts[i] + '!!!' + 'foo' + 'bar';
     i++;
   }
   return newFacts;
