ed68838c342898b116f3ed128c64aa2f3145d53a
Travis Exline <t.exline22@gmail.com>
2019-04-08 16:44:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var allMusicians = [];
+  for (var i=0; i < musicians.length; i++) {
+    allMusicians.push(players[i] + " plays " + instruments[i]);
+  }
+  return allMusicians;
+}
