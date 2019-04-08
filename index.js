cc980a1a590c17fed9c77058d1aa0a44ebd53153
Travis Exline <t.exline22@gmail.com>
2019-04-08 16:57:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,15 +8,12 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts() {
-  var facts = [
-    "foo",
-    "bar"
-  ];
-  var newFacts = [];
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var array = [];
   var i = 0;
-  while (newFacts.length < facts.length) {
-    newFacts.push(facts[i] + "!!!");
+  while (array.length < facts.length) {
+    array.push(facts[i] + "!!!");
     i++;
   }
-  return newFacts;
+  return array;
 }
